422b23060f8f54120c6aa541ae71ffecb3f7d6a7
Gregor Best <gbe@intepi.net>
2009-07-25 17:24:15 +0200

    dwm-theme: increase border width

--- themes/dwm/dwm.lua
+++ themes/dwm/dwm.lua
@@ -18,7 +18,7 @@ return {
     fg_focus      = "#ffffff",
     fg_urgent     = "#111111",
 
-    border_width  = 1,
+    border_width  = 2,
     border_normal = "#cccccc",
     border_focus  = "#0066ff",
 
