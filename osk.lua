5d4f963c70204c33a63f817a1210e3428637a777
Gregor Best <gbe@intepi.net>
2009-11-22 14:57:09 +0100

    osk: add gestures, make a bit more modular, add toggle widget
    Signed-off-by: Gregor Best <gbe@intepi.net>

--- osk.lua
+++ osk.lua
@@ -2,6 +2,7 @@
 --   * Original by farhaven
 
 -- Grab the environment
+local dbg      = require("dbg")
 local wibox    = require("awful.wibox")
 local layout   = require("awful.widget.layout")
 local button   = require("awful.button")
@@ -9,38 +10,94 @@ local util     = require("awful.util")
 local ipairs   = ipairs
 local tostring = tostring
 local table    = table
+local unpack   = unpack
+local math     = {
+    abs        = math.abs
+}
 local capi     = {
     widget     = widget,
     fake_input = root.fake_input
 }
 
-
 -- On Screen Keyboard for the awesome window manager
 module("osk")
 
+local font = "Fixed 16"
+local pressed_key = ""
+
+local keymaps = {
+    letters = {
+        { "q", "w", "e", "r", "t", "z", "u", "i", "o" },
+        { "a", "s", "d", "f", "g", "h", "j", "l", "p" },
+        { "y", "x", "c", "v", "b", "n", "m", "k", "⏎" },
+    }
+}
+local active_keymap = "letters"
 
 local keycodes = {
-    q=24,     w=25, e=26, r=27, t=28, z=52, u=30, i=31, o=32, p=33, Ret=36,
+    q=24,     w=25, e=26, r=27, t=28, z=52, u=30, i=31, o=32, p=33, ["⏎"]=36,
     a=38,     s=39, d=40, f=41, g=42, h=43, j=44, k=45, l=46,
     ["<"]=94, y=29, x=53, c=54, v=55, b=56, n=57, m=58, [","]=59, ["."]=60, ["/"]=61,
 }
 
-local function create_button_row(...)
+local function distance(k1, k2, map)
+    local p1 = { }
+    local p2 = { }
+    if k1 == k2 then return { x = 0, y = 0 } end
+    for y, row in ipairs(map) do
+        for x, sym in ipairs(row) do
+            if k1 == sym then
+                p1 = { x = x, y = y }
+            end
+            if k2 == sym then
+                p2 = { x = x, y = y }
+            end
+            if p1.x and p2.x then break end
+        end
+        if p1.y and p2.y then break end
+    end
+    return { x = p1.x - p2.x, y = p1.y - p2.y }
+end
+
+local function fake_key(keycode)
+    capi.fake_input("key_press", keycode)
+    capi.fake_input("key_release", keycode)
+end
+
+local function keypress(keysym)
+    pressed_key = keysym
+end
+
+local function keyrelease(keysym)
+    local d = distance(keysym, pressed_key, keymaps[active_keymap])
+    dbg.dump(d)
+    if d.x < -1 then -- "BackSpace"
+        fake_key(22)
+    elseif d.x > 1 then -- "Space"
+        fake_key(65)
+    elseif d.y > 1 then -- "Return"
+        fake_key(36)
+    else
+        fake_key(keycodes[keysym])
+    end
+    pressed_key = ""
+end
+
+local function create_button_row(keys)
     local widgets = { layout = layout.horizontal.flex }
 
-    for k, v in ipairs(arg) do
+    for k, v in ipairs(keys) do
         local w = capi.widget({ type = "textbox" })
         w:margin({ top = 10, left = 10, right = 10, bottom = 10 })
-        w.border_width = 1
+        w.border_width = 2
         w.border_color = "#1E2320"
         w.bg           = "#4F4F4F"
         w.text_align   = "center"
-        w.text = util.escape(tostring(v))
+        w.text = "<span font_desc=\"" .. font .. "\">" .. util.escape(tostring(v)) .. "</span>"
         w:buttons(util.table.join(
-            button({ }, 1, nil, function ()
-                capi.fake_input("key_press", keycodes[v])
-                capi.fake_input("key_release", keycodes[v])
-            end)
+            button({ }, 1,
+                function () keypress(v) end,
+                function () keyrelease(v) end)
         ))
 
         table.insert(widgets, w)
@@ -49,13 +106,47 @@ local function create_button_row(...)
     return widgets
 end
 
+local function create_keymap(map)
+    local w = { layout = layout.vertical.flex }
+    for _, row in ipairs(map) do
+        table.insert(w, create_button_row(row))
+    end
+    return w
+end
+
 local w = wibox({
-    height   = 100,
+    height   = 160,
     position = "bottom",
-    widgets  = {
-        { create_button_row("q", "w", "e", "r", "t", "z", "u", "i", "o", "p", "Ret") },
-        { create_button_row("a", "s", "d", "f", "g", "h", "j", "k", "l") },
-        { create_button_row("<", "y", "x", "c", "v", "b", "n", "m", ",", ".", "/") },
-        layout = layout.vertical.flex
-    }
+    widgets  = { create_keymap(keymaps["letters"]), layout = layout.horizontal.leftright }
 })
+w.visible = false
+
+function show()
+    w.visible = true
+end
+
+function hide()
+    w.visible = false
+end
+
+function visible()
+    return w.visible
+end
+
+function widget()
+    local w_toggle = capi.widget({ type = "textbox" })
+    w_toggle.text = "<span color=\"#FF0000\">⌨</span>"
+    w_toggle:margin({ left = 20, right = 20 })
+    w_toggle:buttons(util.table.join(
+        button({ }, 1, function ()
+            if visible() then
+                hide()
+                w_toggle.text = "<span color=\"#FF0000\">⌨</span>"
+            else
+                show()
+                w_toggle.text = "<span color=\"#00FF00\">⌨</span>"
+            end
+        end)
+    ))
+    return w_toggle
+end
