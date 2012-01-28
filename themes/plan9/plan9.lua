f681fad146168a9911c03e13f7e8bb9777b833f8
Gregor Best <gbe@ring0.de>
2012-01-28 21:58:36 +0100

    add plan9 color scheme
    Signed-off-by: Gregor Best <gbe@ring0.de>

---
+++ themes/plan9/plan9.lua
@@ -0,0 +1,34 @@
+-----------------------------
+-- Plan9 theme for Awesome --
+-----------------------------
+
+return {
+    -- font          = "Monospace 8",
+    -- font          = "fixed 8",
+    -- font          = "Sans 8",
+    font          = "Terminus 8",
+    -- font          = "FreeMono 9",
+    -- font          = "Droid Sans Mono 8",
+
+    bg_normal     = "#9fefef", -- done
+    bg_focus      = "#57a8a8", -- done
+    bg_urgent     = "#478847", -- done
+
+    fg_normal     = "#000000", -- done
+    fg_focus      = "#000000", -- done
+    fg_urgent     = "#000000", -- done
+
+    border_width  = 4,
+    border_normal = "#9fefef", -- done
+    border_focus  = "#57a8a8",
+
+    taglist_squares_sel    = "~/.config/awesome/themes/dwm/tasklist_f.png",
+    taglist_squares_unsel  = "~/.config/awesome/themes/dwm/tasklist_u.png",
+    tasklist_floating_icon = "~/.config/awesome/themes/dwm/floating.png",
+
+    titlebar_close_button_normal = "/usr/local/share/awesome/themes/default/titlebar/close.png",
+    titlebar_close_button_focus  = "/usr/local/share/awesome/themes/default/titlebar/closer.png",
+
+    menu_submenu_icon = "/usr/local/share/awesome/themes/default/submenu.png",
+    awesome_icon = "/usr/local/share/awesome/icons/awesome16.png"
+}
